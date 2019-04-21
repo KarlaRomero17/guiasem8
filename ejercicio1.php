d25fabe3dfc0bb616937cd72b10f0c37da20795f
KarlaRomero17 <romerolissette17@gmail.com>
2019-04-20 18:27:09 -0600

    Se agrego al archivo index.php un echo de "Hola mundo".

--- ejercicio1.php
+++ ejercicio1.php
@@ -1,13 +1,12 @@
 <?php
-
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo ("Holaa mundo!");
 
-
+/*Desarrolle un programa que encuentre el factorial de un número; con ingresos de
+datos desde el teclado*/
 
 
 
--- ejercicio2.php
+++ ejercicio2.php
@@ -5,9 +5,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-echo ("Holaa mundo!");
 
-
-
-
+//Desarrolle un programa Java que sume los numeros del 1 al 100 (ambos inclusive)
 
